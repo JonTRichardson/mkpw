abc2670630fc98efcf9d5818cf3858121894fd7f
Jon T Richardson <JonRichardsonBama@gmail.com>
2020-07-17 16:36:01 -0600

    Added getRandomWord() method.  Added default word file.  Changed test case names.  Added to __doc__.
    Default word file is from  http://www.manythings.org/vocabulary/lists/l/
    
     Changes to be committed:
    	new file:   defaultWords.txt
    	modified:   mkpw.py

--- mkpw.py
+++ mkpw.py
@@ -2,7 +2,10 @@ import random
 import unittest
 
 class MkPw:
-    """ WIP Password maker.  Caveat: This doesn't fully address the cryptographic security of the RNG."""
+    """ WIP Password maker.  Caveat: This doesn't fully address the cryptographic security of the RNG. """
+    """ For now, asks the user to press enter.  My assumption is that this will set the RNG seed to    """
+    """ something truly random because it is dependent on user input.  """
+    wordList = []
 
     def getRandomDigit(self):
         List = [ '1', '2', '3', '4', '5', '6', '7', '8', '9', '0' ]
@@ -17,7 +20,6 @@ class MkPw:
         random.seed()
         choice = random.choice(List)
         return choice
-    
 
     def getRandomLetterUC(self):
         List = [ 'A', 'B', 'C', 'D', 'E', 'F', 'G', 'H', 'I', 'J', 'K', 'L', 'M', 'N', 'O', 'P', 'Q', 'R','S','T','U','V','W','X','Y','Z' ]
@@ -26,25 +28,43 @@ class MkPw:
         choice = random.choice(List)
         return choice
 
+    ########################################################################################################################
+    ##   getRandomWord()
+    ##      For now, uses default word file.  This is for dev only.  I wouldn't recommend actually using this file.
+    def getRandomWord(self):
+        if ( self.wordList == [] ):
+            with open('defaultWords.txt') as file:
+                wordList = list(file.read().split())
+
+        input( "press enter to generate a random word" )
+        random.seed()
+        choice = random.choice( wordList )
+        return choice
+
 
 import string
 
 class TestMkPw( unittest.TestCase ):
     x = MkPw()
 
-    def test1(self):
+    def testDigit(self):
         d = self.x.getRandomDigit()
         self.assertTrue( d in string.digits )
 
-    def test2(self):
+    def testLower(self):
         a = self.x.getRandomLetterLC()
         self.assertTrue( a in string.ascii_lowercase )
 
-    def test3(self):
+    def testUpper(self):
         a = self.x.getRandomLetterUC()
         self.assertTrue( a in string.ascii_uppercase )
 
+    def testWord(self):
+        word = self.x.getRandomWord()
+        self.assertTrue( len(word)>=2 and len(word)<8 )
 
+        for c in word:
+            self.assertTrue( c in string.ascii_lowercase )
 
     
 if __name__ == '__main__':
