1215ce7e788022371a883115d29432fecb468de4
Jon T Richardson <JonRichardsonBama@gmail.com>
2020-08-20 11:15:34 -0600

    Derived class overriding getRandomWord()
    	modified:   genpw.py

--- genpw.py
+++ genpw.py
@@ -8,9 +8,27 @@ argumentParser = argparse.ArgumentParser()
 argumentParser.add_argument( "filename", type=str )
 arguments = argumentParser.parse_args()
 
+class myPw(mkpw.MkPw):
+    """ Derived from MkPw, allows user to choose from random set of words. """
+
+    def getRandomWord(self, seed="" ):
+        if (seed == "" ):
+            input( "press enter to generate a random word" )
+            random.seed()
+        else:
+            random.seed( seed )
+        response = "n"
+        while (response != "y"):
+            choice = random.choice( self.wordList )
+            print("Chosen word is ", choice)
+            response = input("Is that ok(y/n?")
+            print("response is ", response)
+        return choice
+
+
 ##############################
 #  Main
-po = mkpw.MkPw( arguments.filename )
+po = myPw( arguments.filename )
 
 methods = [ po.getRandomDigit, po.getRandomLetterLC, po.getRandomLetterUC, po.getRandomSpecial, po.getRandomWord ]
 
